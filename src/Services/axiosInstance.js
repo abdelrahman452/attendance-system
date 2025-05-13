7937438f3cebf7a4294676ee31a973f39a414718
Abdelrahman Mohamed <abdelrahman.mobarak@namaa-il.com>
2025-05-13 16:33:34 +0300

    add context

--- src/Services/axiosInstance.js
+++ src/Services/axiosInstance.js
@@ -10,14 +10,3 @@ const axiosInstance=
     })
 
 export default axiosInstance
-// // axiosInstance.js
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//   baseURL: import.meta.env.VITE_API_BASE, // Correct key: baseURL, not baseUrl
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// export default axiosInstance;
